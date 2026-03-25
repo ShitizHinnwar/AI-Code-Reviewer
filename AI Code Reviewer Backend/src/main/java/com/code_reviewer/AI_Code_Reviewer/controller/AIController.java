143e551f793b6c2720060565c24b409ad9ecf5d3
Shitiz Hinnwar <shitizhinnwaar2004@gmail.com>
2026-03-25 17:16:16 +0530

    Fixed CORS issue

--- AI Code Reviewer Backend/src/main/java/com/code_reviewer/AI_Code_Reviewer/controller/AIController.java
+++ AI Code Reviewer Backend/src/main/java/com/code_reviewer/AI_Code_Reviewer/controller/AIController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api")
-@CrossOrigin("*")
+@CrossOrigin(origins = "*")
 public class AIController {
 
     @Autowired
